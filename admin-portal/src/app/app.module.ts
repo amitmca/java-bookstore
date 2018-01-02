a5ca954b30a690aca4a864b6384ae2c2a185c472
Piyush Patel <piyushpatel2005@gmail.com>
2018-01-03 00:17:56 +0530

    Create navbar

--- admin-portal/src/app/app.module.ts
+++ admin-portal/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
